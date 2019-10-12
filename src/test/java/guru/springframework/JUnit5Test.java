b530b20b7ecaaa8fc66ac7ecf625d6ec4dac6acf
rhaughey <rfhaughey@gmail.com>
2019-10-12 15:03:17 -0400

    Adding JUnit5 to the mix

---
+++ src/test/java/guru/springframework/JUnit5Test.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by rfh on 10/12/2019
+ */
+public class JUnit5Test {
+
+    @Test
+    void exampleTest() {
+        System.out.println("Test...");
+    }
+}
