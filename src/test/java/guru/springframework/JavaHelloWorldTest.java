ec2ebea5a1ebc22a110662a8efe79069a17610a6
rhaughey <rfhaughey@gmail.com>
2019-10-17 16:33:11 -0400

    Testing with TestNG

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by rfh on 10/17/2019
+ */
+public class JavaHelloWorldTest {
+    @Test
+    public void testGetHello() {
+
+    }
+}
