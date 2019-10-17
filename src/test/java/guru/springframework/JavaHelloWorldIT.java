661a0f6d34a4929eb77d3d0e82100c6200d3d13f
rhaughey <rfhaughey@gmail.com>
2019-10-17 17:04:29 -0400

    Failsafe for Integration Tests

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by rfh on 10/17/2019
+ */
+public class JavaHelloWorldIT {
+
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT ran");
+    }
+}
